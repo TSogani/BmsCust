2be165ad5e7643fe562374ce2a68b4fc97a20d24
devendra <devendra@192.168.0.9>
2017-09-29 00:12:25 +0530

    dev test

--- src/com/poc/test/TopCls.java
+++ src/com/poc/test/TopCls.java
@@ -14,7 +14,7 @@ public class TopCls {
 		EISCDTest test= context.getBean("eiscdTest",EISCDTest.class);
 		
 		test.m1();
-		
+		System.out.println("dev test");
 		long endTime   = System.currentTimeMillis();
 		long totalTime = endTime - startTime;
 		System.out.println("total taken time : "+totalTime/1000);
